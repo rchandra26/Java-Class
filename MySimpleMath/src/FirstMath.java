81dd03bb26e8aa20d4e947f900bb4e53b5cc9d70
MyGitHub2626 <MyGitHub2626@users.noreply.github.com>
2020-07-06 18:04:15 -0700

    my simple math java

---
+++ MySimpleMath/src/FirstMath.java
@@ -0,0 +1,14 @@
+
+public class FirstMath {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 20, y = 30, sum, mul;
+sum = x + y;
+mul = x * y;
+
+System.out.println("sum of these numbers: "+sum);
+System.out.println("product of these numbers:" +mul);
+	}
+
+}
