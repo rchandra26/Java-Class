51d2410a7ee7508fb92538ff1ded68e48fbce246
MyGitHub2626 <MyGitHub2626@users.noreply.github.com>
2020-07-21 17:07:50 -0700

    if block project

---
+++ IfBlockProject/src/IfBlock.java
@@ -0,0 +1,16 @@
+
+public class IfBlock {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int number = 13;
+		if(number % 2==1) {
+			System.out.println("odd");
+		} else {
+			System.out.println("even");
+		}
+
+	}
+}
+
