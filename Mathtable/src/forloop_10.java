8a9966707805520987ebd7511e9d6bead64cdafb
MyGitHub2626 <MyGitHub2626@users.noreply.github.com>
2020-07-21 17:02:40 -0700

    My for loops program

---
+++ Mathtable/src/forloop_10.java
@@ -0,0 +1,12 @@
+
+public class forloop_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 100;
+		for( ; x <=100; x = x+10) {
+	System.out.println(x);
+}
+	}
+
+}
